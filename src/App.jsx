95a04cf059325c680aba5ef6a8dfeb2030295ae7
Marcos Mourão <dev@mourao.info>
2026-06-17 02:11:43 -0300

    feat: add random word selection

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,45 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [pickedWord, setPickedWord] = useState("");
+  const [letters, setLetters] = useState([]);
 
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // Pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { category, word };
+  }
   
   const startGame = () => {
+    // Pick word and pick category
+    const { category, word } = pickWordAndCategory();
+
+    // Create an array of letters
+    let wordLetters = word.split("")
+
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word);
+    console.log(wordLetters);
+
+
+    // Fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
     setGameStage(stages[1].name);
+
+
+
+
+
   }
 
   //process the letter input
